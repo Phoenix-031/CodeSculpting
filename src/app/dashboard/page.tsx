dcd189ba61de72d0f76eab42e22702b96c90acdf
asrvd <asheeshh@duck.com>
2024-03-20 18:47:20 +0530

    add cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,54 @@
+import {
+  Card,
+  CardContent,
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+const questions = [
+  {
+    title: "Guess the word",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis cumque culpa excepturi labore ducimus voluptate! Earum corporis nesciunt eius!",
+  },
+  {
+    title: "CSS Challenge 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis cumque culpa excepturi labore ducimus voluptate! Earum corporis nesciunt eius!",
+  },
+  {
+    title: "CSS Challenge 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis cumque culpa excepturi labore ducimus voluptate! Earum corporis nesciunt eius!",
+  },
+];
+
 export default function Dashboard() {
-  return <p>Dashboard</p>;
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center h-full w-full">
+      <h2 className="text-4xl text-foreground text-center text-balance">
+        Challenges ⚡
+      </h2>
+      <div className="flex gap-4 justify-center items-center">
+        {questions.map((question, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <CardTitle>{question.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>{question.description}</CardDescription>
+            </CardContent>
+            <CardFooter>
+              <Button
+              className="w-full"
+              >Start</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="flex justify-between items-center bg-primary/10 -skew-x-[10deg] p-4 px-6 border border-primary/80 border-dotted lg:w-2/3 w-full">
+    <header className="flex justify-between items-center bg-primary/10 p-4 px-6 border border-primary/80 border-dotted lg:w-2/3 w-full">
       <Link href={"/"}>
         <h1 className="text-2xl text-primary-foreground font-bold">
           Code Sculptor
